59cd4f060d982bebbef3708337357e3cfb078d45
Rick Mohamed <rick.mohamed@student.moringascool.com>
2023-09-08 12:01:28 +0300

    new commit

--- lib/main.py
+++ lib/main.py
@@ -1,6 +1,8 @@
 # main.py
 
 import argparse
+import random
+from datetime import datetime, timedelta
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base, WeatherEntry
@@ -13,8 +15,27 @@ def create_database():
     print("Database created.")
 
 def populate_database():
-    # Code to populate the database with weather data
-    pass
+    session = Session()
+    towns = ["Nairobi", "Mombasa", "Kisumu", "Eldoret", "Nakuru", "Meru", "Thika", "Kakamega", "Kericho", "Malindi"]
+    
+    # Generate random weather data for each town
+    for town in towns:
+        date = datetime(2023, 8, 1)
+        end_date = datetime(2023, 8, 31)
+        
+        while date <= end_date:
+            time = date.strftime("%H:%M:%S")
+            temperature = random.randint(15, 35)  # Random temperature between 15°C and 35°C
+            cloud_cover = random.choice(["Clear", "Partly Cloudy", "Cloudy", "Rainy"])
+            
+            entry = WeatherEntry(town=town, date=date.strftime("%Y-%m-%d"), time=time,
+                                 temperature=temperature, cloud_cover=cloud_cover)
+            session.add(entry)
+            
+            date += timedelta(hours=2)  # Increment date by 2 hours
+        
+    session.commit()
+    print("Weather data populated.")
 
 def list_weather_entries(town, date):
     session = Session()
